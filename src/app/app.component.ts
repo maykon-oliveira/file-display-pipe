4e91cdb1646729021e76b5eda32977afaa19dc35
Maykon Oliveira <maykonoliveira850@gmail.com>
2021-06-03 22:15:05 -0300

    Publicaçao github pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'file-display-pipe';
+  files: File[] = [];
+
+  handleOnlyImages(event: any) {
+    const mimeType = event.target.files[0].type;
+
+    if (mimeType.match(/image\/*/) == null) {
+      alert('Apenas imagens são suportados');
+      return;
+    }
+
+    this.files = event.target.files;
+  }
+
+  handleOnly2Images(event: any) {
+    if (!event.target.files[0] || event.target.files.length > 2) {
+      alert('No máximo dois arquivos');
+      return;
+    }
+
+    const mimeType = event.target.files[0].type;
+
+    if (mimeType.match(/image\/*/) == null) {
+      alert('Apenas imagens são suportados');
+      return;
+    }
+
+    this.files = event.target.files;
+  }
+
+  handleOnlyPngImages(event: any) {
+    const mimeType = event.target.files[0].type;
+
+    if (mimeType.match(/image\/png/) == null) {
+      alert('Apenas imagens PNG são suportados');
+      return;
+    }
+
+    this.files = event.target.files;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FileDisplayPipe } from './file-display.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileDisplayPipe
   ],
   imports: [
     BrowserModule
